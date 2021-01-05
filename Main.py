0ad97f7d999729b455340d0048c9055320deac5f
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2021-01-05 13:08:03 +0530

    Code Done

---
+++ Main.py
@@ -0,0 +1,46 @@
+import math
+def add_nums(x, y):
+    print(x + y)
+
+def subtract_nums(x, y):
+    print(x - y)
+
+def multiply_nums(x, y):
+    print(x * y)
+
+def divide_nums(x, y):
+    print(x / y)
+
+def power_nums(x, y):
+    print(x ** y)
+
+def squareRoot_nums(x):
+    print(math.sqrt(x))
+
+usrinp = input("\033[1;32;1mAddition, Subtraction, Multiplication, Division, Power, Square Root\n>>> ")
+
+if usrinp == "Addition":
+    num1 = int(input("\033[1;31;1mFirst Number: "))
+    num2 = int(input("\033[1;36;1mSecond Number: "))
+    add_nums(num1, num2)
+elif usrinp == "Subtraction":
+    num1 = int(input("\033[1;31;1mFirst Number: "))
+    num2 = int(input("\033[1;36;1mSecond Number: "))
+    subtract_nums(num1,num2)
+elif usrinp == "Multiplication":
+    num1 = int(input("\033[1;31;1mFirst Number: "))
+    num2 = int(input("\033[1;36;1mSecond Number: "))
+    multiply_nums(num1, num2)
+elif usrinp == "Division":
+    num1 = int(input("\033[1;31;1mFirst Number: "))
+    num2 = int(input("\033[1;36;1mSecond Number: "))
+    divide_nums(num1, num2)
+elif usrinp == "Power":
+    num1 = int(input("\033[1;31;1mNumber: "))
+    num2 = int(input("\033[1;36;1mTo the power of:"))
+    power_nums(num1,num2)
+elif usrinp == "Square Root":
+    num1 = int(input("\033[1;33;1mNumber: "))
+    squareRoot_nums(num1)
+
+print("Thanks For Using JaiCalc")
