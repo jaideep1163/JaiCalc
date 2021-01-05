27c5712cec1fa52d026783982030175135d2aebd
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2021-01-05 13:12:05 +0530

    Code Finished?

--- Main.py
+++ Main.py
@@ -22,25 +22,31 @@ usrinp = input("\033[1;32;1mAddition, Subtraction, Multiplication, Division, Pow
 if usrinp == "Addition":
     num1 = int(input("\033[1;31;1mFirst Number: "))
     num2 = int(input("\033[1;36;1mSecond Number: "))
+    print("\033[1;32;1m")
     add_nums(num1, num2)
 elif usrinp == "Subtraction":
     num1 = int(input("\033[1;31;1mFirst Number: "))
     num2 = int(input("\033[1;36;1mSecond Number: "))
+    print("\033[1;32;1m")
     subtract_nums(num1,num2)
 elif usrinp == "Multiplication":
     num1 = int(input("\033[1;31;1mFirst Number: "))
     num2 = int(input("\033[1;36;1mSecond Number: "))
+    print("\033[1;32;1m")
     multiply_nums(num1, num2)
 elif usrinp == "Division":
     num1 = int(input("\033[1;31;1mFirst Number: "))
     num2 = int(input("\033[1;36;1mSecond Number: "))
+    print("\033[1;32;1m")
     divide_nums(num1, num2)
 elif usrinp == "Power":
     num1 = int(input("\033[1;31;1mNumber: "))
     num2 = int(input("\033[1;36;1mTo the power of:"))
+    print("\033[1;32;1m")
     power_nums(num1,num2)
 elif usrinp == "Square Root":
     num1 = int(input("\033[1;33;1mNumber: "))
+    print("\033[1;32;1m")
     squareRoot_nums(num1)
 
 print("Thanks For Using JaiCalc")
